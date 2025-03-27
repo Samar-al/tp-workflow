81d6d8395b1b0e22ee3e49687c511549ae705b53
Samar-al <samar.alkhalilchebaye@gmail.com>
2025-03-27 16:21:29 +0100

    fix calculator2

--- src/main/java/tpworkflow/Main.java
+++ src/main/java/tpworkflow/Main.java
@@ -12,12 +12,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        List<Article> articles = new ArrayList<>();
+       final  List<Article> articles = new ArrayList<>();
         articles.add(new Article("Clavier", 30.0, 2));
         articles.add(new Article("Souris", 15.0, 1));
         articles.add(new Article("Écran", 200.0, 1));
 
-        double total = InvoiceCalculator.calculateTotalInvoice(articles, 100.0, 0.10); // 10% de réduction si > 100€
+        final double total = InvoiceCalculator.calculateTotalInvoice(articles, 100.0, 0.10); // 10% de réduction si > 100€
         System.out.printf("Montant total à payer : %.2f €%n", total);
     }
     public static int square(int x) {
